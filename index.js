601fa50769970adf824f74298caecf7bd9b0c19e
PhumudzoSly <phumumahandana@gmail.com>
2022-05-16 21:21:57 +0200

    Updated

--- index.js
+++ index.js
@@ -1,74 +1,71 @@
-export const validateIdNumber = (idNumber) => {
-  // regex pattern using the Luhn algorithm
-  const ex =
+const validateID = (id) => {
+  const regex =
     /^(((\d{2}((0[13578]|1[02])(0[1-9]|[12]\d|3[01])|(0[13456789]|1[012])(0[1-9]|[12]\d|30)|02(0[1-9]|1\d|2[0-8])))|([02468][048]|[13579][26])0229))(( |-)(\d{4})( |-)(\d{3})|(\d{7}))/;
 
-  // if the ID number is not valid
-  if (ex.test(idNumber) === false) {
+  if (regex.test(id) === false) {
     return {
       valid: false,
+      // message: "Invalid ID number",
     };
   }
 
   return {
+    isCitizen: getCitizenship(id),
+    gender: getGender(id),
+    dateOfBirth: getDateOfBirth(id),
     valid: true,
-    gender: getGender(idNumber),
-    DOB: getDateOfBirth(idNumber),
-    isCitizen: getCitizenship(idNumber),
   };
 };
 
-// Find gender from ID number
-export const getGender = (idNumber) => {
-  const genderCode = idNumber.substring(6, 10);
-  const gender = Number(genderCode) < 5000 ? "female" : "male";
-  return gender;
-};
-
-// find Citizenship from ID number
-export const getCitizenship = (idNumber) => {
-  const citizenshipCode = idNumber.substring(10, 11);
-  const isCitizen = Number(citizenshipCode) === 0;
-  return isCitizen;
-};
-
 // find Date of Birth from ID number
-export const getDateOfBirth = (idNumber) => {
-  // get year, and assume the century
+export const getDateOfBirth = (id) => {
+  //
+
+  const currentDay = new Date().getDay();
+  const currentMonth = new Date().getMonth();
   const currentYear = new Date().getFullYear();
   const currentCentury = Math.floor(currentYear / 100) * 100;
-  const currentMonth = new Date().getMonth();
-  const currentDay = new Date().getDay();
 
-  let yearPart = currentCentury + parseInt(idNumber.substring(0, 2), 10);
-
-  const monthPart = parseInt(idNumber.substring(2, 4), 10) - 1;
-
-  const dayPart = parseInt(idNumber.substring(4, 6), 10);
+  const day = parseInt(id.substring(4, 6), 10);
+  const month = parseInt(id.substring(2, 4), 10) - 1;
+  let year = currentCentury + parseInt(id.substring(0, 2), 10);
 
   // only 16 years and above are eligible for an ID
-  const eligibleYear = currentYear - 16;
-  // make sure the ID's DOB is not below 16 years from today, if so it's last century issue
+  const validYear = currentYear - 16;
+
   if (
-    yearPart > eligibleYear ||
-    (yearPart === eligibleYear &&
-      (monthPart > currentMonth ||
-        (monthPart === currentMonth && dayPart > currentDay)))
+    year > validYear ||
+    (year === validYear &&
+      (month > currentMonth || (month === currentMonth && day > currentDay)))
   ) {
-    yearPart -= 100; // must be last century
+    year -= 100; // must be last century
   }
 
-  const dateOfBirth = new Date(yearPart, monthPart, dayPart);
+  const DOB = new Date(year, month, day);
 
   // validate that date is in a valid range by making sure that it wasn't 'corrected' during construction
   if (
-    !dateOfBirth ||
-    dateOfBirth.getFullYear() !== yearPart ||
-    dateOfBirth.getMonth() !== monthPart ||
-    dateOfBirth.getDate() !== dayPart
+    !DOB ||
+    DOB.getDate() !== day ||
+    DOB.getMonth() !== month ||
+    DOB.getFullYear() !== year
   ) {
     return undefined;
   }
 
-  return dateOfBirth;
+  return DOB;
+};
+
+// find Citizenship from ID number
+const getCitizenship = (id) => {
+  const code = id.substring(10, 11);
+  const validCitizen = Number(code) === 0;
+  return validCitizen;
+};
+
+// Find gender from ID number
+const getGender = (id) => {
+  const code = id.substring(6, 10);
+  const gender = Number(code) < 5000 ? "female" : "male";
+  return gender;
 };
